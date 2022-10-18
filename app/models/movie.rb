e6c724bd5b8ee8b6bd11031a779eca063d14fef6
aaron wertheim <aawertheim@gmail.com>
2022-10-18 14:26:23 -0500

    movie details

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ApplicationRecord
     has_many :watchlist_movies
     has_many :users, through: :watchlist_movies
 
+    has_many :reviews
+    has_many :users, through: :reviews
+
 end
---
+++ client/src/Components/MovieDetails.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function MovieDetails() {
+
+    const [movie, setMovie] = useState([])
+    const {id} = useParams();
+
+    useEffect(() => {
+        fetch(`/movies/${id}`)
+        .then(r => r.json())
+        .then(movieData => setMovie(movieData))
+    },[id])
+
+    return(
+        <div>
+            <div>{movie.name} Details</div>
+            <div>{movie.genre?.map(movie => movie + " ")}</div>
+            <div>{movie.description}</div>
+            <img src={movie.image} />
+        </div>
+    )
+}
+export default MovieDetails;
