670aff1dd5b032c70667ca6d6c33558d8f96ae0f
aaron wertheim <aawertheim@gmail.com>
2022-10-19 06:01:50 -0500

    fixed like btn

--- client/src/Components/MovieDetails.js
+++ client/src/Components/MovieDetails.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ReviewCard from "./ReviewCard";
 
 function MovieDetails({user}) {
 
     const [movie, setMovie] = useState([]);
-    const [votes, setVotes] = useState([])
     const { id } = useParams();
 
     useEffect(() => {
@@ -13,26 +13,8 @@ function MovieDetails({user}) {
         .then(movieData => setMovie(movieData))
     },[id])
 
-    useEffect(() => {
-        fetch('/votes')
-        .then(r => r.json())
-        .then(voteData => setVotes(voteData.filter(vote => vote.review_id === parseInt(id)).length))
-    },[id])
+   
 
-    function likeReview(rev) {
-
-        
-        fetch('/votes', {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ 
-                user_id: user.id,
-                review_id: rev.id
-            }),
-        }).then(setVotes(votes + 1))
-    }
 
     return(
         <div>
@@ -42,12 +24,7 @@ function MovieDetails({user}) {
             <img src={movie.image} />
             <div>Reviews:
                 {movie.reviews?.map(review => (
-                    <div>
-                        <div>Author: {review.author}</div> 
-                        <div>Rating: {review.rating}</div>
-                        <div>Review: {review.content}</div>
-                        <div><button onClick={() => likeReview(review)}>👍</button> {votes}</div>
-                    </div>
+                    <ReviewCard review={review} user={user} />
                 ))}
             </div>
         </div>
