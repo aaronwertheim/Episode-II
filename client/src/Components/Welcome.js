4405385080190edde796835ae016ce393bc1f342
aaron wertheim <aawertheim@gmail.com>
2022-10-22 18:39:53 -0500

    movie catalog page

--- client/src/Components/Welcome.js
+++ client/src/Components/Welcome.js
@@ -17,7 +17,7 @@ function Welcome() {
                 <img src={ randomMovieImage } alt="" loading="lazy" width="" height="" />
                 </div>
                 <div class="md:7/12 lg:w-6/12">
-                <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Nuxt development is carried out by passionate developers</h2>
+                <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Welcome to Episode II</h2>
                 <p class="mt-6 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
                 <p class="mt-4 text-gray-600"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
                 </div>
